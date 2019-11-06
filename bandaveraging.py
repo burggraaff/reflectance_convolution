a1a4febc85c5d99631a506937952c626bd161e0c
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-11-06 18:58:24 +0100

    Use trapz for uneven spacing

--- bandaveraging.py
+++ bandaveraging.py
@@ -16,17 +16,15 @@ def split_spectrum(data_table, label):
 def bandaverage(band_wavelengths, band_response, data_wavelengths, data_response):
     response_interpolated = np.interp(band_wavelengths, data_wavelengths, data_response, left=0, right=0)
     response_multiplied = response_interpolated * band_response
-    wavelength_step = band_wavelengths[1] - band_wavelengths[0]
-    response_sum = wavelength_step * response_multiplied.sum()
-    weight_sum = wavelength_step * band_response.sum()
+    response_sum = np.trapz(response_multiplied, x=band_wavelengths)
+    weight_sum = np.trapz(band_response, x=band_wavelengths)
     response_average = response_sum / weight_sum
     return response_average
 
 def bandaverage_multi(band_wavelengths, band_response, data_wavelengths, data_response_multi):
     response_interpolated = np.array([np.interp(band_wavelengths, data_wavelengths, data_response, left=0, right=0) for data_response in data_response_multi])
     response_multiplied = response_interpolated * band_response
-    wavelength_step = band_wavelengths[1] - band_wavelengths[0]
-    response_sum = wavelength_step * response_multiplied.sum(axis=1)
-    weight_sum = wavelength_step * band_response.sum()
+    response_sum = np.trapz(response_multiplied, x=band_wavelengths, axis=1)
+    weight_sum = np.trapz(band_response, x=band_wavelengths)
     response_average = response_sum / weight_sum
     return response_average
