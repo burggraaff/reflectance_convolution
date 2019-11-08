177211b745e4772c638480fd3c61dea0c5b3bf4e
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-11-08 13:10:48 +0100

    OLI

--- oli.py
+++ oli.py
@@ -3,25 +3,24 @@ Generate boxcar and gaussian spectral response functions
 """
 
 import numpy as np
-from matplotlib import pyplot as plt
 from bandaveraging import load_data, bandaverage_multi, boxplot_absolute, boxplot_relative
+from matplotlib import pyplot as plt
 
 wavelengths_data, Ed, Lw, R_rs = load_data()
 
-bands = [1,2,3,4,8]
-band_labels = [f"ETM+\nband {band}" for band in bands]
-responses = [np.loadtxt(f"spectral_response/ETM+/spectral_b{x}.dat", skiprows=3, unpack=True) for x in bands]
-colours = ["b", "g", "r", "xkcd:dark red", "k"]
+band_labels = ["CA", "Blue", "Green", "Red", "NIR", "Pan"]
+responses = [np.loadtxt(f"spectral_response/OLI/{band}.txt", skiprows=1, unpack=True, usecols=[0,1]) for band in band_labels]
+colours = ["xkcd:dark blue", "xkcd:blue", "xkcd:green", "xkcd:red", "xkcd:dark brown", "k"]
 
-for response, band, colour in zip(responses, bands, colours):
-    plt.plot(response[0], response[1], label=f"ETM+ band {band}", c=colour)
+for response, band, colour in zip(responses, band_labels, colours):
+    plt.plot(response[0], response[1], label=f"OLI band {band}", c=colour)
 plt.xlim(400, 920)
 plt.ylim(0, 1.01)
 plt.xlabel("Wavelength [nm]")
 plt.ylabel("Relative response")
 plt.legend(loc="best")
 plt.grid(ls="--", color="0.5")
-plt.savefig(f"results/ETM+_bands.pdf")
+plt.savefig(f"results/OLI_bands.pdf")
 plt.show()
 
 reflectance_space = np.array([bandaverage_multi(response[0], response[1], wavelengths_data, R_rs) for response in responses])
@@ -29,5 +28,5 @@ radiance_space = np.array([bandaverage_multi(response[0], response[1], wavelengt
 difference_absolute = reflectance_space - radiance_space
 difference_relative = 100*difference_absolute / radiance_space
 
-boxplot_relative(difference_relative, colours=colours, band_labels=band_labels, sensor_label="ETM+")
-boxplot_absolute(difference_absolute, colours=colours, band_labels=band_labels, sensor_label="ETM+")
+boxplot_relative(difference_relative, colours=colours, band_labels=band_labels, sensor_label="OLI")
+boxplot_absolute(difference_absolute, colours=colours, band_labels=band_labels, sensor_label="OLI")
