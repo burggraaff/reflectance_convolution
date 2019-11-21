0895b954071c5b1014a959f03a745b146a5e259e
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-11-21 12:06:56 +0100

    Double boxplot

--- bandaveraging.py
+++ bandaveraging.py
@@ -94,6 +94,43 @@ def calculate_median_and_errors(differences):
     upper_error = upper_percentile - medians
     return medians, lower_error, upper_error
 
+def double_boxplot(data, label="", unit="", sensor_label="", band_labels=None, colours=None):
+    if band_labels is None:
+        band_labels = [""] * len(data)
+    if colours is None:
+        colours = ["k"] * len(data)
+
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, data.shape[1]//3), tight_layout=True, sharey=True, gridspec_kw={"hspace": 0, "wspace": 0})
+    bplot_main = axs[0].boxplot(data.T, vert=False, showfliers=False, whis=[5,95], patch_artist=True, labels=band_labels)
+    for patch, colour in zip(bplot_main["boxes"], colours):
+        patch.set_facecolor(colour)
+    bplot_zoom = axs[1].boxplot(data.T, vert=False, showfliers=False, whis=[5,95], patch_artist=True, labels=band_labels)
+    for patch, colour in zip(bplot_zoom["boxes"], colours):
+        patch.set_facecolor(colour)
+
+    for ax in axs:
+        ax.set_xlabel(f"Difference [{unit}]")
+        ax.grid(ls="--", color="0.5")
+
+    axs[0].set_title("Tara Med.: "+sensor_label)
+
+    xlim = axs[0].get_xlim()
+    if xlim[0] > 0:
+        axs[0].set_xlim(xmin=0)
+    elif xlim[-1] < 0:
+        axs[0].set_xlim(xmax=0)
+
+    axs[1].tick_params(axis="y", left=False, labelleft=False)
+
+    if label == "rel": # relative plot
+        axs[1].set_xlim(-5.1, 5.1)
+    else: # absolute plot
+        axs[1].set_xlim(-30.1, 30.1)
+
+    plt.savefig(f"results/Tara_med/{sensor_label}_{label}_double.pdf")
+    plt.show()
+    plt.close()
+
 def make_boxplot(data, label="", unit="", sensor_label="", band_labels=None, colours=None):
     if band_labels is None:
         band_labels = [""] * len(data)
@@ -128,6 +165,7 @@ def boxplot_relative(differences, band_labels=None, sensor_label="", **kwargs):
         print(f"{sensor_label} {band} band: {med:+.2f} (+{up:.2f}, -{low:.2f}) %")
 
     make_boxplot(differences, label="rel", unit="%", sensor_label=sensor_label, band_labels=band_labels, **kwargs)
+    double_boxplot(differences, label="rel", unit="%", sensor_label=sensor_label, band_labels=band_labels, **kwargs)
 
 def boxplot_absolute(differences, band_labels=None, sensor_label="", scaling_exponent=6, **kwargs):
     if band_labels is None:
@@ -141,3 +179,4 @@ def boxplot_absolute(differences, band_labels=None, sensor_label="", scaling_exp
         print(f"{sensor_label} {band} band: {med:+.2f} (+{up:.2f}, -{low:.2f}) x 10^-6 sr^-1")
 
     make_boxplot(differences_scaled, label="abs", unit=unit, sensor_label=sensor_label, band_labels=band_labels, **kwargs)
+    double_boxplot(differences_scaled, label="abs", unit=unit, sensor_label=sensor_label, band_labels=band_labels, **kwargs)
