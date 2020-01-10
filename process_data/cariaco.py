a8e5f9ce5c315fc7ddc513af0ffe7c211f62fec6
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-01-10 15:47:35 +0100

    Finalise data processing

--- process_data/cariaco.py
+++ process_data/cariaco.py
@@ -18,14 +18,6 @@ for file in files:
         else:
             break
 
-    # reject data with negative Rrs
-    if len(np.where(Rrs < 0)[0]) > 0:
-        continue
-
-    # reject data that do not cover the full spectral range
-    if 380 not in wavelengths:
-        continue
-
     date, time, lon, lat = find_auxiliary_information_seabass(file)
 
     cols = ["Date", "Time", "Latitude", "Longitude"] + [f"Ed_{wvl:.0f}" for wvl in wavelengths] + [f"R_rs_{wvl:.0f}" for wvl in wavelengths]
@@ -46,27 +38,46 @@ for Ed_k, R_rs_k in zip(Ed_keys, R_rs_keys):
     data[Ed_k] = data[Ed_k].to(u.watt / (u.m**2 * u.nm))
 
     data[R_rs_k].unit = 1 / u.steradian
-    # Normalise to R_rs at 748 nm
-    data[R_rs_k] = data[R_rs_k] - normalisation
 
     Lw = data[f"Ed_{wavelength}"] * data[f"R_rs_{wavelength}"]
     Lw.name = f"Lw_{wavelength}"
     Lw.unit = u.watt / (u.m**2 * u.nm * u.steradian)
     data.add_column(Lw)
 
-# Remove columns that are consistently negative in R_rs
-for wvl in wavelengths[wavelengths >= 680]:
-    remove_keys = [f"{s}_{wvl:.0f}" for s in ("Ed", "Lw", "R_rs")]
-    data.remove_columns(remove_keys)
-
+# Remove rows with NaN values
 R_rs_keys = [key for key in data.keys() if "R_rs" in key]
-remove_indices = [i for i, row in enumerate(data) if any(row[key] > 0.2 for key in R_rs_keys)]
+remove_indices = [i for i, row_mask in enumerate(data.mask) if any(row_mask[key] for key in R_rs_keys)]
 data.remove_rows(remove_indices)
-print(f"Removed {len(remove_indices)} rows with R_rs > 0.2")
+print(f"Removed {len(remove_indices)} rows with NaN values")
 
+# Remove rows with missing R_rs values (< -90)
+remove_indices = [i for i, row in enumerate(data) if any(row[key] < -90 for key in R_rs_keys)]
+data.remove_rows(remove_indices)
+print(f"Removed {len(remove_indices)} rows with R_rs < -90")
+
+# Remove rows with missing Ed values (<)
 remove_indices = [i for i, row in enumerate(data) if row["Ed_600"] <= 0.1]
 data.remove_rows(remove_indices)
-print(f"Removed {len(remove_indices)} rows with missing values")
+print(f"Removed {len(remove_indices)} rows with missing Ed values")
+
+# Remove rows with unphysically high R_rs values (> 0.02)
+remove_indices = [i for i, row in enumerate(data) if any(row[key] > 0.02 for key in R_rs_keys)]
+data.remove_rows(remove_indices)
+print(f"Removed {len(remove_indices)} rows with R_rs > 0.02")
+
+# Clip small negative values (0 > R_rs > -1e-4) to 0
+R_rs_keys = [key for key in data.keys() if "R_rs" in key]
+Lw_keys = [key for key in data.keys() if "Lw" in key]
+for Lw_k, R_rs_k in zip(Lw_keys, R_rs_keys):
+    ind = np.where((data[R_rs_k] < 0) & (data[R_rs_k] > -1e-4))
+    data[R_rs_k][ind] = 0
+    data[Lw_k][ind] = 0
+
+# Remove rows with negative R_rs
+R_rs_keys = [key for key in data.keys() if "R_rs" in key]
+remove_indices = [i for i, row in enumerate(data) if any(row[key] < 0 for key in R_rs_keys)]
+data.remove_rows(remove_indices)
+print(f"Removed {len(remove_indices)} rows with negative values")
 
 map_data(data, data_label="CARIACO", projection='gnom', lat_0=10.5, lon_0=-64.67, llcrnrlon=-70, urcrnrlon=-59, llcrnrlat=5, urcrnrlat=15, resolution="h", parallels=np.arange(4, 16, 2), meridians=np.arange(-70, -56, 2))
 
