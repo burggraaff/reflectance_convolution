b22e91a8c2c6ff6c04950c314953fbd8709de06a
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-01-13 01:05:40 +0100

    Load all data at once

--- process_data/all_spectra.py
+++ process_data/all_spectra.py
@@ -7,25 +7,22 @@ from sba.data_processing import split_spectrum
 folder = Path("data")
 
 all_data_files = sorted(folder.glob("*processed.tab"))
-colours = ["xkcd:royal blue", "xkcd:orange", "xkcd:lime green", "xkcd:magenta", "xkcd:olive green", "xkcd:navy", "xkcd:maroon", "xkcd:bright pink", "xkcd:hunter green", "xkcd:vomit", "xkcd:chocolate", "xkcd:rust brown", "xkcd:crimson", "xkcd:black"]
 
-nr_spectra = 0
+data_all = [read(file) for file in all_data_files]
+N_all = [len(data) for data in data_all]
+N = sum(N_all)
+
+labels = [file.stem[:-10] for file in all_data_files]
 
 fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, tight_layout=True, gridspec_kw={"wspace":0, "hspace":0}, figsize=(5,7))
 
-for file in all_data_files:
-    data = read(file)
-    label = file.stem[:-10]
-    nr_spectra += len(data)
-
+for data, label in zip(data_all, labels):
     wavelengths, Ed = split_spectrum(data, "Ed")
     wavelengths, Lw = split_spectrum(data, "Lw")
     wavelengths, R_rs = split_spectrum(data, "R_rs")
 
     for ax, spectrum in zip(axs.ravel(), [Ed, Lw, R_rs]):
         ax.plot(wavelengths, spectrum.T, c="k", alpha=0.01, zorder=1)
-
-    print(label)
 
 for ax, label in zip(axs.ravel(), ["$E_d$", "$L_w$", "$R_{rs}$"]):
     ax.set_ylabel(label)
@@ -38,7 +35,7 @@ axs[0].set_ylim(ymin=0)
 axs[1].set_ylim(ymin=0)
 axs[2].set_ylim(ymin=0)
 
-axs[0].set_title(f"All spectra ({nr_spectra})")
+axs[0].set_title(f"All spectra ({N})")
 plt.savefig(f"data/plots/spectra_all_data.pdf")
 plt.show()
 plt.close()
@@ -46,18 +43,13 @@ plt.close()
 # Zoom
 fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, tight_layout=True, gridspec_kw={"wspace":0, "hspace":0}, figsize=(5,7))
 
-for file in all_data_files:
-    data = read(file)
-    label = file.stem[:-10]
-
+for data, label in zip(data_all, labels):
     wavelengths, Ed = split_spectrum(data, "Ed")
     wavelengths, Lw = split_spectrum(data, "Lw")
     wavelengths, R_rs = split_spectrum(data, "R_rs")
 
     for ax, spectrum in zip(axs.ravel(), [Ed, Lw, R_rs]):
         ax.plot(wavelengths, spectrum.T, c="k", alpha=0.01, zorder=1)
-
-    print(label)
 
 for ax, label in zip(axs.ravel(), ["$E_d$", "$L_w$", "$R_{rs}$"]):
     ax.set_ylabel(label)
@@ -70,7 +62,7 @@ axs[0].set_ylim(0, 2)
 axs[1].set_ylim(0, 0.015)
 axs[2].set_ylim(0, 0.01)
 
-axs[0].set_title(f"All spectra ({nr_spectra})")
+axs[0].set_title(f"All spectra ({N})")
 plt.savefig(f"data/plots/spectra_all_data_zoom.pdf")
 plt.show()
 plt.close()
