3afcfafa64de77ed87525cdbe63c65542ad1e4a6
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-01-13 14:47:17 +0100

    Refactor interpolation & Use NaN for edges

--- sba/bandaveraging.py
+++ sba/bandaveraging.py
@@ -4,27 +4,36 @@ Module with functions for band-averaging
 
 import numpy as np
 
+
+def interpolate_spectral_data(band_wavelengths, data_wavelengths, data_response, extrapolation_value=np.nan):
+    data_interpolated = np.interp(band_wavelengths, data_wavelengths, data_response, left=extrapolation_value, right=extrapolation_value)
+    return data_interpolated
+
+
 def bandaverage(band_wavelengths, band_response, data_wavelengths, data_response):
-    response_interpolated = np.interp(band_wavelengths, data_wavelengths, data_response, left=0, right=0)
+    response_interpolated = interpolate_spectral_data(band_wavelengths, data_wavelengths, data_response)
     response_multiplied = response_interpolated * band_response
     response_sum = np.trapz(response_multiplied, x=band_wavelengths)
     weight_sum = np.trapz(band_response, x=band_wavelengths)
     response_average = response_sum / weight_sum
     return response_average
 
+
 def bandaverage_multi(band_wavelengths, band_response, data_wavelengths, data_response_multi):
-    response_interpolated = np.array([np.interp(band_wavelengths, data_wavelengths, data_response, left=0, right=0) for data_response in data_response_multi])
+    response_interpolated = np.array([interpolate_spectral_data(band_wavelengths, data_wavelengths, data_response) for data_response in data_response_multi])
     response_multiplied = response_interpolated * band_response
     response_sum = np.trapz(response_multiplied, x=band_wavelengths, axis=1)
     weight_sum = np.trapz(band_response, x=band_wavelengths)
     response_average = response_sum / weight_sum
     return response_average
 
+
 def calculate_differences(reflectance_space, radiance_space):
     difference_absolute = reflectance_space - radiance_space
     difference_relative = 100 * difference_absolute / radiance_space
 
     return difference_absolute, difference_relative
+
 
 def calculate_median_and_errors(differences):
     lower_percentile = np.nanpercentile(differences, 15.9, axis=1)
