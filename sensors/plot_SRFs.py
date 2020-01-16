b6611797ae6ffeac1425c2ab00ecb012e521b536
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-01-16 19:13:10 +0100

    Smaller plot

--- sensors/plot_SRFs.py
+++ sensors/plot_SRFs.py
@@ -2,6 +2,7 @@
 Plot all spectral response functions (SRFs) separately, and unique ones together
 """
 
+import numpy as np
 from matplotlib import pyplot as plt
 from sba.response_curves import load_all_sensors, load_selected_sensors
 
@@ -13,7 +14,7 @@ for sensor in sensors_all:
 
 # Make a combined plot of selected sensors
 sensors_selected = load_selected_sensors("etm+", "oli", "spectacle", "seawifs", "viirs", "modis", "msi", "meris", "olci")
-fig, axs = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=False, tight_layout=True, figsize=(10,5), gridspec_kw={"hspace": 0, "wspace": 0})
+fig, axs = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=False, tight_layout=True, figsize=(7,3), gridspec_kw={"hspace": 0, "wspace": 0})
 for sensor, ax in zip(sensors_selected, axs.T.ravel()):
     sensor.plot(ax)
     ax.tick_params(axis="y", left=False, labelleft=False)
@@ -22,7 +23,7 @@ for ax in axs[:-1].ravel():
     ax.tick_params(axis="x", bottom=False, labelbottom=False)
 for ax in axs[-1].ravel():
     ax.set_xlabel("Wavelength [nm]")
-    ax.tick_params(axis="x", rotation=40)
+    ax.set_xticks(np.arange(250, 1500, 250))
 
 for ax in axs[:,0].ravel():
     ax.set_ylabel("RSR")
@@ -34,9 +35,9 @@ axs[2,0].set_yticks([0, 0.25, 0.5, 0.75, 1])
 
 axs[0,0].set_xlim(320, 1320)
 
-axs[0,0].set_title("ETM+, OLI, SPECTACLE")
-axs[0,1].set_title("SeaWiFS, VIIRS, MODIS A")
-axs[0,2].set_title("Sentinel-2A, MERIS, Sentinel-3A")
+axs[0,0].set_title("ETM+, OLI,\nSPECTACLE")
+axs[0,1].set_title("SeaWiFS, VIIRS,\nMODIS Aqua")
+axs[0,2].set_title("Sentinel-2A, MERIS,\nSentinel-3A")
 plt.savefig("spectral_response/SRF_all.pdf")
 plt.show()
 plt.close()
