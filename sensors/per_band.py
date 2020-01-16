7431ea7126f54152648926ea0d758caacd36c209
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-01-16 21:35:21 +0100

    Boxplot per band

---
+++ sensors/per_band.py
@@ -0,0 +1,56 @@
+from sba.bandaveraging import calculate_differences
+from sba.io import load_data_file
+from sba.response_curves import load_all_sensors
+from pathlib import Path
+from matplotlib import pyplot as plt
+import warnings
+
+sensors = load_all_sensors()
+
+data_files = Path("data").glob("*processed.tab")
+
+labels, wavelengths, Eds, Lws, R_rss = zip(*[load_data_file(file) for file in data_files])
+
+def boxplot(band, diff_abs, diff_rel, labels, saveto="boxplot.pdf", sensor_name=""):
+    fig, axs = plt.subplots(nrows=2, figsize=(7,3), tight_layout=True, sharex=True, gridspec_kw={"hspace": 0, "wspace": 0})
+    for diff, ax in zip([diff_abs, diff_rel], axs):
+        diffs_including_all = [[x for y in diff for x in y], *diff]
+        labels_all = ["all", *labels]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            bplot = ax.boxplot(diffs_including_all, showfliers=False, whis=[5,95], patch_artist=True, labels=labels_all)
+        for patch in bplot["boxes"]:
+            patch.set_facecolor(band.colour)
+
+        ax.grid(ls="--")
+        ax.axhline(0, c="k", ls="--")
+        ax.tick_params(axis="x", rotation=90)
+
+    axs[0].set_ylabel("[$10^{-6}$ sr$^-1$]")
+    axs[1].set_ylabel("$\Delta R_{rs}$ [%]")
+
+    axs[0].set_title(f"{sensor_name}, {band}")
+
+    fig.align_labels()
+
+    plt.savefig(saveto)
+    plt.show()
+    plt.close()
+
+
+for sensor in sensors:
+    print(sensor)
+    for band in sensor.bands:
+        print(f"     {band}")
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            RrsR = [band.convolve(wavelengths_data, R_rs) for wavelengths_data, R_rs in zip(wavelengths, R_rss)]
+            RrsL = [band.convolve(wavelengths_data, Lw)/band.convolve(wavelengths_data, Ed) for wavelengths_data, Ed, Lw in zip(wavelengths, Eds, Lws)]
+
+            difference_absolute, difference_relative = zip(*[calculate_differences(reflectance_space, radiance_space) for reflectance_space, radiance_space in zip(RrsR, RrsL)])
+
+        # Convert to 10^-6 sr
+        difference_absolute = [1e6 * diff for diff in difference_absolute]
+
+        boxplot(band, difference_absolute, difference_relative, labels, saveto=f"results/per_band/{sensor.name}_{band}.pdf", sensor_name=sensor.name)
