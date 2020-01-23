69b6701bdca561291e023ec9d4f7c7d0490f15f6
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-01-23 17:19:47 +0100

    Title

--- process_data/all_spectra.py
+++ process_data/all_spectra.py
@@ -127,6 +127,8 @@ ax_zoom.set_xlim(320, 1000)
 ax_zoom.set_ylim(0, 0.008)
 ax_N.set_xlim(300, 1340)
 
+ax_Lw.set_title(f"All spectra ($N = {N_total}$)")
+
 plt.savefig("data/plots/spectra_all_data_combined.png", dpi=300)
 plt.show()
 plt.close()
