8111139ab0549ce6ed4d2d02660472b644033906
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-02-13 11:43:47 +0100

    Prettier plot

--- chla/satellite_algorithms_all.py
+++ chla/satellite_algorithms_all.py
@@ -14,14 +14,15 @@ for file in data_files:
     for difference, absrel in zip([difference_absolute, difference_relative], ["abs", "rel"]):
         unit = "mg m$^{-3}$" if absrel == "abs" else "%"
 
-        plt.figure(figsize=(7,2.5), tight_layout=True)
-        bp = plt.boxplot(difference, whis=[5,95], showfliers=False, labels=satellite_algorithm_labels, patch_artist=True)
+        plt.figure(figsize=(7,1))
+        bp = plt.boxplot(difference, whis=[5,95], widths=0.4, showfliers=False, labels=satellite_algorithm_labels, patch_artist=True)
         for patch in bp["boxes"]:
             patch.set_facecolor("xkcd:dark green")
         plt.ylabel(f"$\Delta$ Chl-a [{unit}]")
         plt.grid(ls="--")
         plt.axhline(0, ls="--", c="k")
-        plt.tick_params(axis="x", rotation=15)
+        plt.tick_params(axis="x", rotation=12)
+        plt.locator_params(axis="y", nbins=5)
         plt.title(f"Convolution error in Chl-a estimates, {label} data")
-        plt.savefig(f"results/{label}/{label}_chla_{absrel}.pdf")
+        plt.savefig(f"results/{label}/{label}_chla_{absrel}.pdf", bbox_inches="tight")
         plt.show()
